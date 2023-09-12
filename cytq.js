2b5c12e1da2daf01a2b9c4406d1459a6641cf3a7
Emyorii <59774025+Emyori@users.noreply.github.com>
2023-09-12 14:30:48 +0800

    Update cytq.js

--- cytq.js
+++ cytq.js
@@ -1,121 +1,37 @@
-/*
+// 2023-07-11 08:35
 
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
-苹果商店下载: 彩云天气
-
-修改内容：解锁会员 
-
-注意事项：每次解锁需启动圈叉或loon
-
-
-本地重写
-
-^https:\/\/biz\.caiyunapp\.com\/v2\/user.+ url script-response-body cytq.js
-
-
-
-主机名MITM
-
-biz.caiyunapp.com
-
-
-*/
-
-var obj = JSON.parse($response.body); 
-// 
-
-obj ={
-  "result": {
-    "xy_vip_expire": 0,
-    "vip_expired_at": 0,
-    "ranking_above": 99,
-    "auto_renewal_type": "",
-    "third_party_id": null,
-    "svip_expired_at": 4093515016.946474,
-    "is_login": true,
-    "is_xy_vip": false,
-    "platform_name": "caiyun",
-    "xy_svip_expire": 0,
-    "third_party_ids": [],
-    "score": 2000,
-    "is_xy_auto_renewal": false,
-    "is_primary": true,
-    "phone_num": "13800138000",
-    "free_trial": 0,
-    "last_acted_at": 1626863905.591368,
-    "vip_type": "s",
-    "is_phone_verified": true,
-    "wt": {
-      "ranking_above": 99,
-      "vip": {
-        "enabled": true,
-        "is_auto_renewal": false,
-        "auto_renewal_type": "",
-        "svip_auto_renewal_type": "",
-        "svip_expired_at": 4093515016.946474,
-        "expired_at": 0
-      },
-      "last_acted_at": 1626863905.591368,
-      "created_at": 1626863905.591368,
-      "is_login": true
-    },
-    "device_id": "B409FB39-AAC3-46E3-80B6-7544B5BB8C27",
-    "name": "鹤归何晚",
-    "bound_status": {
-      "qq": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "weibo": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "google": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "apple": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "weixin": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "caiyun": {
-        "username": "",
-        "is_bound": true,
-        "id": "60f7f9217112ca00110b567f"
-      },
-      "twitter": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
-      },
-      "facebook": {
-        "username": "",
-        "is_bound": false,
-        "id": ""
+if (url.includes("/v1/vip_info")) {
+  if (obj.vip) {
+    obj.vip.expires_time = "4030000000";
+  }
+  if (obj.svip) {
+    obj.svip.expires_time = "4030000000";
+  }
+} else if (url.includes("/v2/user")) {
+  if (obj.result) {
+    obj.result.svip_given = 730;
+    obj.result.is_phone_verified = true;
+    obj.result.is_xy_vip = true;
+    obj.result.vip_expired_at = 4030000000.16;
+    obj.result.is_vip = true;
+    obj.result.xy_svip_expire = 4030000000.16;
+    if (obj.result.wt) {
+      if (obj.result.wt.vip) {
+        obj.result.wt.vip.enabled = true;
+        obj.result.wt.vip.expired_at = 4030000000.16;
+        obj.result.wt.vip.svip_expired_at = 4030000000.16;
       }
-    },
-    "created_at": 1626863905.590722,
-    "is_auto_renewal": false,
-    "hasBeenInvited": false,
-    "platform_id": "",
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNjBmN2Y5MjE3MTEyY2EwMDExMGI1NjdmIiwic3ZpcF9leHBpcmVkX2F0IjoxNjYzMTUxOTQwLjk0NjQ3NCwidmlwX2V4cGlyZWRfYXQiOjB9.ezQpaDFL1T_WCIZq42Q71hLgGa9DYDBaz0OjDzeuf2I",
-    "svip_given": 0,
-    "avatar": "https://caiyunapp.com/imgs/webtrs/default.png",
-    "is_vip": true,
-    "gender": "0",
-    "_id": "60f7f9217112ca00110b567f"
-  },
-  "rc": 0
+      obj.result.wt.svip_given = 730;
+    }
+    obj.result.is_primary = true;
+    obj.result.xy_vip_expire = 4030000000.16;
+    obj.result.svip_expired_at = 4030000000.16;
+    obj.result.vip_type = "s";
+  }
 }
 
-$done({body:JSON.stringify(obj)});
-// 
+$done({ body: JSON.stringify(obj) });
